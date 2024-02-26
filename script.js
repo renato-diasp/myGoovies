5ca264f440d21390d2f634b443897567d299f798
renato-diasp <64383656+renato-diasp@users.noreply.github.com>
2024-02-26 10:52:47 -0300

    Adicionado recurso de tradução do nome do filme pesquisado e sinpse

--- script.js
+++ script.js
@@ -5,10 +5,15 @@ const movieYear = document.getElementById('year-movie');
 
 async function insertieMovieData(){   
   try{
-    let url = `https://www.omdbapi.com/?apikey=${key}&t=${generateMovieParameter()}${generateYearParameter()}`;  
+    let url = `https://www.omdbapi.com/?apikey=${key}&t=${await generateMovieParameter()}${generateYearParameter()}`;  
     const response =  await fetch(url);
     const data = await response.json();
-    if(data.Error) throw new Error('Filme não encontrado')    
+    if(data.Error) throw new Error('Filme não encontrado');
+
+    let urlTranslate = `https://api.mymemory.translated.net/get?q=${data.Plot}!&langpair=en|pt-br`;
+    const responseTranslate = await fetch(urlTranslate);
+    const dataTranslate = await responseTranslate.json();
+    const plotTranslate = dataTranslate.responseData.translatedText;
   
     movieContainer.innerHTML = `
       <div id="movie-info" class="d-flex container">
@@ -16,7 +21,7 @@ async function insertieMovieData(){
           <img src="${data.Poster}" alt="${data.Title}">
         </div>
         <div id="movie-data" class="d-flex flex-column">
-           <p>Sinopse: ${data.Plot}</p>
+           <p>Sinopse: ${plotTranslate}</p>
            <p>Elenco: ${data.Actors}</p>
             <p>Gênero: ${data.Genre}</p>
         </div>
@@ -33,7 +38,12 @@ async function insertieMovieData(){
 function generateMovieParameter (){
   if(movieName.value === '') throw new Error('Nome do filme deve ser informado');   
    
-  const searchMovie = movieName.value.split(' ').join('+')
+  let urlTranslate = `https://api.mymemory.translated.net/get?q=${movieName.value}!&langpair=pt-br|en`;
+  const responseTranslate = await fetch(urlTranslate);
+  const dataTranslate = await responseTranslate.json();
+  const nameMovieTranslate = dataTranslate.responseData.translatedText;   
+   
+  const searchMovie = nameMovieTranslate.split(' ').join('+')
   return searchMovie;
 }
 
